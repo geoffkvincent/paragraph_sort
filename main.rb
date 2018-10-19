7d7db475921dc6c8ae37b81ec0068a62d79b89ab
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-19 10:18:31 -0600

    aded loop

--- main.rb
+++ main.rb
@@ -4,8 +4,13 @@ def para_sort
   puts "Enter Paragraph"
   para = gets.gsub(/[^a-zA-Z ]/,'').gsub(/ +/,' ')
   para_arr = para.split(' ')
+  counts = Hash.new 0
+
+  para_arr.each do |p|
+  counts[p] += 1
+end
   
-  puts para_arr
+  puts counts
 end
 
 para_sort
